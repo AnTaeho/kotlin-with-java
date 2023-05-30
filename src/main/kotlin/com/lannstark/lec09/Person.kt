03df23117f9008dc84e057cca7adcb2b7bca998f
antaeho <an981022@naver.com>
2023-05-30 14:07:55 +0900

    9강 정리

---
+++ src/main/kotlin/com/lannstark/lec09/Person.kt
@@ -0,0 +1,40 @@
+package com.lannstark.lec09
+
+fun main() {
+    val person = Person("ANTaeHo", 100)
+    println(person.name)
+    person.age = 10
+    println(person.age)
+
+    val person2 = Person("AAA")
+}
+
+class Person(
+    name: String = "안태호",
+    var age: Int = 1
+) {
+
+    var name = name
+        set(value) {
+            field = value.uppercase()
+        }
+
+    val uppercaseName: String
+        get() = this.name.uppercase()
+
+    init {
+        if (age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
+        }
+    }
+
+    //하지만 디폴트 파라미터를 권장
+    constructor(name: String): this(name, 1)
+
+    fun isAdult(): Boolean {
+        return this.age >= 20
+    }
+
+    val isAdult: Boolean
+        get() = this.age >= 20
+}
