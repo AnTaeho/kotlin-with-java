8a16a1fea0784cad96d7210092b2c9b1f708a585
antaeho <an981022@naver.com>
2023-05-30 20:35:01 +0900

    12강 정리

---
+++ src/main/kotlin/com/lannstark/lec12/Person.kt
@@ -0,0 +1,31 @@
+package com.lannstark.lec12
+
+fun main() {
+    println(Singleton.a)
+    Singleton.a += 10
+    println(Singleton.a)
+
+}
+
+class Person private constructor(
+    var name: String,
+    var age: Int
+) {
+    //동행 객체
+    companion object Factory : Log{
+        private const val MIN_AGE = 1
+
+        @JvmStatic
+        fun newBaby(name: String): Person {
+            return Person(name, MIN_AGE)
+        }
+
+        override fun log() {
+            println("나는 Person 동행 객체입니다.")
+        }
+    }
+}
+
+object Singleton {
+    var a: Int = 0
+}
